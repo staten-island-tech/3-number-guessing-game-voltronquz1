04cf4a0bd0f4d55fd487cf49cbe42646e9d0f4a5
a <justinc1127@nycstudents.net>
2025-10-17 09:34:20 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+# number = 1
+# while number > 0:
+#     print("This will never stop!")
+
+# order = ""
+
+# while order != "done":
+#     order = input("What would you like to order? (type 'done' to finish): ")
+
+# print("Thanks for your order!")
+
+Num=10
+while Num>=0:
+    Num=Num-1
+print(Num)
+
+# def guessing():
+#     import random
+#     actual=random.randint(1,10)
+#     guess=int(input("Guess a number from 1-10"))
+#     while guess !=actual:
+#         if guess > 10 or guess <1:
+
+ 
