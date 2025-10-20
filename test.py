f803077f97657ca2e7ba610ceed4868523a8e655
a <justinc1127@nycstudents.net>
2025-10-19 23:16:42 -0400

    Update test.py

--- test.py
+++ test.py
@@ -25,18 +25,45 @@
 import random
 numbers=[1,2,3,4,5,6,7,8,9,10]
 wrongnums=[]
-wrongnum=random.choice(numbers)
+randomnum=random.choice(numbers)
 
 guessnum=int(input("guess num from 1-10"))
-while guessnum != wrongnum:
-    if guessnum > wrongnum:
-        wrongnum.append(wrongnums)
+while guessnum != randomnum:
+    if guessnum > randomnum:
+        wrongnums.append(guessnum)
         print(f"The numbers {wrongnums} are not correct")
-    elif guessnum < wrongnum:
-        wrongnum.append(wrongnums)
+    elif guessnum < randomnum:
+        wrongnums.append(guessnum)
         print("Your number is greater than the random number")
-    elif guessnum >wrongnum:
+    elif guessnum >randomnum:
+        wrongnums.append(guessnum)
         print("Your number is less than the random number")
-        wrongnum.append(wrongnums)
-    elif guessnum ==wrongnum:
+if guessnum ==randomnum:
+        wrongnums.append(wrongnums)
         print("your number is correct")
+        print("You guess correctly, the number was", randomnum)
+        print("Your guess history:", wrongnums)
+
+
+# import random
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# wrong_numbers = []
+# random_number = random.choice(numbers)
+
+# guessed_number = int(input("Guess the number between 1 and 10: "))
+
+# while guessed_number != random_number:
+#     if guessed_number > random_number:
+#         wrong_numbers.append(guessed_number)
+#         print(f"Number too big (You already guessed {wrong_numbers})")
+#         guessed_number = int(input("Try lower: "))
+
+#     elif guessed_number < random_number:
+#         wrong_numbers.append(guessed_number)
+#         print(f"Number too small (You already guessed {wrong_numbers})")
+#         guessed_number = int(input("Try higher: "))
+
+# if guessed_number == random_number:
+#     wrong_numbers.append(random_number)
+#     print("You guess correctly, the number was", random_number)
+#     print("Your guess history:", wrong_numbers)
