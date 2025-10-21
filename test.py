d60e9129013f38837bab4c2c90f0eea58cb251c8
a <justinc1127@nycstudents.net>
2025-10-21 08:37:17 -0400

    Update test.py

--- test.py
+++ test.py
@@ -31,13 +31,13 @@ guessnum=int(input("guess num from 1-10"))
 while guessnum != randomnum:
     if guessnum > randomnum:
         wrongnums.append(guessnum)
-        print(f"The numbers {wrongnums} are not correct")
+        print(f"The number {wrongnums} are not correct")
+        print("Your number is less than the random number")
     elif guessnum < randomnum:
         wrongnums.append(guessnum)
+        print(f"The number {wrongnums} are not correct")
         print("Your number is greater than the random number")
-    elif guessnum >randomnum:
-        wrongnums.append(guessnum)
-        print("Your number is less than the random number")
+        
 if guessnum ==randomnum:
         wrongnums.append(wrongnums)
         print("your number is correct")
@@ -45,25 +45,3 @@ if guessnum ==randomnum:
         print("Your guess history:", wrongnums)
 
 
-# import random
-# numbers = [1,2,3,4,5,6,7,8,9,10]
-# wrong_numbers = []
-# random_number = random.choice(numbers)
-
-# guessed_number = int(input("Guess the number between 1 and 10: "))
-
-# while guessed_number != random_number:
-#     if guessed_number > random_number:
-#         wrong_numbers.append(guessed_number)
-#         print(f"Number too big (You already guessed {wrong_numbers})")
-#         guessed_number = int(input("Try lower: "))
-
-#     elif guessed_number < random_number:
-#         wrong_numbers.append(guessed_number)
-#         print(f"Number too small (You already guessed {wrong_numbers})")
-#         guessed_number = int(input("Try higher: "))
-
-# if guessed_number == random_number:
-#     wrong_numbers.append(random_number)
-#     print("You guess correctly, the number was", random_number)
-#     print("Your guess history:", wrong_numbers)
